cf08ca77975392135a56bc755649415d87116125
BrunoHMR <bruno_hmr@outlook.com>
2024-04-18 00:18:03 -0300

    notebook with papermill

--- run_model.sh
+++ run_model.sh
@@ -5,4 +5,4 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path='/home/bruno/repos/3_ciclo_intermediario/3_ds_em_clusterizacao'
 path_to_envs='/home/bruno/anaconda3/envs/ds-insiders/bin'
 
-$path_to_envs/papermill $path/src/models/cluster_insiders_deploy.ipynb $path/reports/cluster_insiders_deploy_$data.ipynb
+$path_to_envs/papermill $path/src/models/cluster_insiders_deploy_aws.ipynb $path/reports/cluster_insiders_deploy_aws_$data.ipynb
