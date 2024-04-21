3cd8d0cf905ee4ffd0fe8e0f03a2262503a47c1d
BrunoHMR <bruno_hmr@outlook.com>
2024-04-21 01:33:22 -0300

    pkl S3

--- run_model.sh
+++ run_model.sh
@@ -6,3 +6,6 @@ path='/home/bruno/repos/3_ciclo_intermediario/3_ds_em_clusterizacao'
 path_to_envs='/home/bruno/anaconda3/envs/ds-insiders/bin'
 
 $path_to_envs/papermill $path/src/models/cluster_insiders_deploy_aws.ipynb $path/reports/cluster_insiders_deploy_aws_$data.ipynb
+
+# if you want to improve the automation create a crontab file with the following example line command:
+# 0 1 * * 0 /usr/bin/bash /home/usr/path/run_model.sh
